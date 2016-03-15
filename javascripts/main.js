f496752e12e31c42e0b91a265d2bb9a015c41a00
Chris Pan <panteng@outlook.com>
2016-03-15 16:05:15 +0800

    Add slider and change skrollr to relative mode.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,30 @@
 require('normalize.css');
 require('font-awesome/css/font-awesome.css');
+require('unslider/dist/css/unslider.css');
 require('../scss/main.scss');
 
 require('smoothscroll-for-websites');
 
 var $ = jQuery = require('jquery');
 var skrollr = require('skrollr');
+var unslider = require('unslider');
 
 $(document).ready(function () {
     skrollr.init({
-        smoothScrolling: false
+        smoothScrolling: false,
+        forceHeight: false
     });
 
-    //$(window).scroll(function () {
-    //    console.log($(window).scrollTop())
-    //});
+    $('.slider').unslider({
+        autoplay: true,
+        speed: 1000,
+        delay: 4000,
+        animation: 'fade',
+        nav: false,
+        arrows: false
+    });
+
+    $(window).scroll(function () {
+        console.log($(window).scrollTop());
+    });
 });
